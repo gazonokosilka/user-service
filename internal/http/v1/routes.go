cadfb5d95c776ea11dda849e2b3c45b852ea5b5a
Gasanov, Denis Gasanov <denis16_08@mail.ru>
2025-11-08 13:30:12 +0300

    add routes

---
+++ internal/http/v1/routes.go
@@ -0,0 +1,31 @@
+package v1
+
+import (
+	"log/slog"
+
+	customerHandler "user-service/internal/http/v1/customer"
+	customerService "user-service/internal/service/customer"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func SetupRoutes(
+	r chi.Router,
+	customerSvc *customerService.Service,
+	log *slog.Logger,
+) {
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+
+	customerH := customerHandler.NewHandler(log, customerSvc)
+
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Route("/customers", func(r chi.Router) {
+			r.Post("/", customerH.CreateCustomer)
+			r.Get("/{id}", customerH.GetCustomer)
+			r.Put("/{id}", customerH.UpdateCustomer)
+		})
+	})
+}
