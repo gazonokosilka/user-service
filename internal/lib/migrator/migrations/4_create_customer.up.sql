b585b9d949eb596aa111c7a580af08515c661df9
Gasanov, Denis Gasanov <denis16_08@mail.ru>
2025-11-08 13:35:53 +0300

    fix migration

--- internal/lib/migrator/migrations/4_create_customer.up.sql
+++ internal/lib/migrator/migrations/4_create_customer.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS "seller_profiles" (
-    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid,
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     "user_data_id" UUID NOT NULL,
     "inn" INTEGER NOT NULL,
     "full_name" VARCHAR(255) NOT NULL,
